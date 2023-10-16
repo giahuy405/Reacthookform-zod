eee1a77241c9fedfd5d99678685a54727c8699dd
Huy Pham <pgiahuy653@gmail.com>
2023-10-16 20:36:59 +0700

    sc

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
 import { useState } from "react";
-import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Input,
+  Select,
+  SelectItem,
+} from "@nextui-org/react";
 import { useForm } from "react-hook-form";
-
+const DAYS = [
+  { label: "Monday", value: "Monday" },
+  { label: "Tuesday", value: "Tuesday" },
+  { label: "Wednesday", value: "Wednesday" },
+  { label: "Thursday", value: "Thursday" },
+];
 function App() {
-  const { register } = useForm();
+  const { register, getValues } = useForm();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(getValues());
+  };
   return (
     <>
       <div className="min-h-screen w-screen flex justify-center pt-8 bg-slate-800">
         <Card className="w-[500px] h-[500px]">
           <CardHeader>Contact form</CardHeader>
           <CardBody>
-            <form>
-              <Input label="Email" size="sm" />
+            <form onSubmit={handleSubmit} className="space-y-5">
+              <Input label="Email" size="sm" {...register("email")} />
+              <Input label="Name" size="sm" {...register("name")} />
+              <Input label="Password" size="sm" {...register("password")} />
+              <Select label="Days" {...register('days')}>
+                {DAYS.map((item) => (
+                  <SelectItem value={item.value} key={item.label}>
+                    {item.label}
+                  </SelectItem>
+                ))}
+              </Select>
+
               <Button type="submit">Submit</Button>
             </form>
           </CardBody>
